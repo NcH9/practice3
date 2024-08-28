5f64e5a5b910cdebcb901d7c14ce2de1164feae5
Nosach Nikolai <Nosach.M.K@gmail.com>
2024-08-28 22:37:07 +0300

    Initial commit

---
+++ prac/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script module>
+export default {
+  name: 'Home',
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <label>Main page</label>
+  </main>
+</template>
